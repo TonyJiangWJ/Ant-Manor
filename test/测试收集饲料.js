05eb648f82a7e80d1bfb898ea6bfd7758b4143ca
TonyJiangWJ <tonyjiangwj@gmail.com>
2022-02-17 23:04:37 +0800

    update to 1.2

--- test/测试收集饲料.js
+++
@@ -1,9 +0,0 @@
-let { config } = require('../config.js')(runtime, global)
-let singletonRequire = require('../lib/SingletonRequirer.js')(runtime, global)
-let commonFunctions = singletonRequire('CommonFunction')
-commonFunctions.ensureAccessibilityEnabled()
-commonFunctions.requestScreenCaptureOrRestart(true)
-let resourceMonitor = require('../lib/ResourceMonitor.js')(runtime, global)
-let collector = require('../core/FodderCollector.js')
-
-collector.exec()
