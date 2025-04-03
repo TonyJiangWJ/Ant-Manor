66c312f9c3399d79710ced4639e017dd4dbbf6f0
TonyJiangWJ <tonyjiangwj@gmail.com>
2025-04-03 19:36:50 +0800

    增加家庭签到相关功能

---
+++ unit/大小号循环家庭签到.js
@@ -0,0 +1,138 @@
+
+let { config } = require('../config.js')(runtime, this)
+let singletonRequire = require('../lib/SingletonRequirer.js')(runtime, this)
+let accountChange = require('../lib/AlipayAccountManage.js')
+let logUtils = singletonRequire('LogUtils')
+let floatyInstance = singletonRequire('FloatyUtil')
+let commonFunctions = singletonRequire('CommonFunction')
+let widgetUtils = singletonRequire('WidgetUtils')
+let runningQueueDispatcher = singletonRequire('RunningQueueDispatcher')
+let fileUtils = singletonRequire('FileUtils')
+let automator = singletonRequire('Automator')
+let unlocker = require('../lib/Unlock.js')
+let WarningFloaty = singletonRequire('WarningFloaty')
+let LogFloaty = singletonRequire('LogFloaty')
+let storageFactory = singletonRequire('StorageFactory')
+let { logInfo, errorInfo, warnInfo, debugInfo, infoLog } = singletonRequire('LogUtils')
+
+let localOcr = require('../lib/LocalOcrUtil.js')
+let DAILY_TASK_DONE = "DAILY_TASK_DONE"
+storageFactory.initFactoryByKey(DAILY_TASK_DONE, { executed: {} })
+let FamilySignRunner = require('../core/FamilySignRunner.js')
+runningQueueDispatcher.addRunningTask()
+// 注册自动移除运行中任务
+commonFunctions.registerOnEngineRemoved(function () {
+  // 重置自动亮度
+  config.resetBrightness && config.resetBrightness()
+  if (config.auto_lock && unlocker.needRelock() === true) {
+    logUtils.debugInfo('重新锁定屏幕')
+    automator.lockScreen()
+  }
+  // 移除运行中任务
+  runningQueueDispatcher.removeRunningTask(true, false,
+    () => {
+      // 保存是否需要重新锁屏
+      unlocker.saveNeedRelock()
+      config.isRunning = false
+    }
+  )
+}, 'main')
+if (!commonFunctions.ensureAccessibilityEnabled()) {
+  errorInfo('获取无障碍权限失败')
+  exit()
+}
+unlocker.exec()
+commonFunctions.requestScreenCaptureOrRestart()
+
+if (!config.main_account || config.accounts.length <= 0) {
+  toastLog('未配置多账号，或账号信息为空，无法进行助力')
+  exit()
+}
+
+if (!floatyInstance.init()) {
+  toast('创建悬浮窗失败')
+  exit()
+}
+floatyInstance.enableLog()
+commonFunctions.showCommonDialogAndWait('切换小号执行家庭签到')
+commonFunctions.listenDelayStart()
+
+// 实时监控截图内容
+require('../lib/WebsocketCaptureHijack.js')()
+
+let currentRunningAccount = ''
+
+
+if (config.accounts && config.accounts.length > 1) {
+
+  // 循环执行签到任务
+  config.accounts.forEach((accountInfo, idx) => {
+    let { account } = accountInfo
+    if (storageFactory.getValueByKey(DAILY_TASK_DONE).executed[account]) {
+      LogFloaty.pushLog('账号' + account + '已执行，跳过')
+      return
+    }
+
+    currentRunningAccount = account
+    floatyInstance.setFloatyText('准备切换账号为：' + account)
+    sleep(1000)
+    accountChange(account)
+    floatyInstance.setFloatyText('切换完毕')
+    sleep(500)
+    floatyInstance.setFloatyText('开始执行签到')
+    try {
+      // 打开首页
+      if (!FamilySignRunner.enterFamily(config.main_account == account)) {
+        errorInfo(['{} 进入家庭失败', account])
+        return
+      }
+      sleep(1000)
+      if (FamilySignRunner.haveNotBeenEnteredAnyFamily()) {
+        return
+      }
+      FamilySignRunner.execSign()
+      FamilySignRunner.openDrawer()
+      if (FamilySignRunner.donateEgg()) {
+        LogFloaty.pushLog('执行了捐蛋 需要重新进入家庭')
+        // 打开首页
+        if (!FamilySignRunner.enterFamily(true)) {
+          errorInfo(['{} 进入家庭失败', account])
+          return
+        }
+      }
+      FamilySignRunner.openDrawer()
+      FamilySignRunner.donateSport()
+      // todo 如果有失败的 不要设置为已完成
+      setExecuted()
+      floatyInstance.setFloatyText('切换下一个账号')
+      sleep(500)
+    } catch (e) {
+      logUtils.errorInfo('执行异常：' + e)
+      floatyInstance.setFloatyText('领取异常 进行下一个')
+    }
+  })
+  floatyInstance.setFloatyText('全部账号签到完毕切换回主账号')
+  sleep(1000)
+  ensureMainAccount()
+  sleep(500)
+} else {
+  logUtils.errorInfo(['当前未配置多账号或账号只有一个，不进行切换'], true)
+}
+commonFunctions.minimize()
+exit()
+
+function ensureMainAccount() {
+  try {
+    accountChange(config.main_account || config.accounts[0])
+  } catch (e) {
+    LogFloaty.pushErrorLog('切换主账号异常' + e)
+    ensureMainAccount()
+  }
+}
+
+
+function setExecuted () {
+  let currentStorage = storageFactory.getValueByKey(DAILY_TASK_DONE)
+  currentStorage.executed[currentRunningAccount] = true
+  storageFactory.updateValueByKey(DAILY_TASK_DONE, currentStorage)
+}
---
+++ unit/家庭投喂.js
@@ -0,0 +1,99 @@
+let { config } = require('../config.js')(runtime, this)
+let singletonRequire = require('../lib/SingletonRequirer.js')(runtime, this)
+let FileUtils = singletonRequire('FileUtils')
+let runningQueueDispatcher = singletonRequire('RunningQueueDispatcher')
+// 代理图片资源回收
+let resourceMonitor = require('../lib/ResourceMonitor.js')(runtime, this)
+let familySinger = require('../core/FamilySignRunner.js')
+let unlocker = require('../lib/Unlock.js')
+let localOcrUtil = require('../lib/LocalOcrUtil.js')
+let OpenCvUtil = require('../lib/OpenCvUtil.js')
+let dateFormat = require('../lib/DateUtil.js')
+let yoloTrainHelper = singletonRequire('YoloTrainHelper')
+let YoloDetection = singletonRequire('YoloDetectionUtil')
+let automator = singletonRequire('Automator')
+let LogFloaty = singletonRequire('LogFloaty')
+let FloatyInstance = singletonRequire('FloatyUtil')
+let NotificationHelper = singletonRequire('Notification')
+FloatyInstance.enableLog()
+let { logInfo, errorInfo, warnInfo, debugInfo, infoLog, debugForDev, clearLogFile, flushAllLogs } = singletonRequire('LogUtils')
+if (!localOcrUtil.enabled) {
+  errorInfo('当前脚本强依赖于OCR，请使用支持OCR的AutoJS版本进行执行')
+  exit()
+}
+// logInfo('======加入任务队列，并关闭重复运行的脚本=======')
+runningQueueDispatcher.addRunningTask()
+
+let widgetUtils = singletonRequire('WidgetUtils')
+let commonFunctions = singletonRequire('CommonFunction')
+// 注册自动移除运行中任务
+commonFunctions.registerOnEngineRemoved(function () {
+  config.resetBrightness && config.resetBrightness()
+  debugInfo('校验并移除已加载的dex')
+  // 移除运行中任务
+  runningQueueDispatcher.removeRunningTask(true, false,
+    () => {
+      // 保存是否需要重新锁屏
+      unlocker.saveNeedRelock()
+      config.isRunning = false
+    }
+  )
+}, 'main')
+
+if (!commonFunctions.ensureAccessibilityEnabled()) {
+  errorInfo('获取无障碍权限失败')
+  exit()
+}
+
+unlocker.exec()
+
+commonFunctions.showCommonDialogAndWait('蚂蚁庄园家庭投喂')
+commonFunctions.listenDelayStart()
+commonFunctions.requestScreenCaptureOrRestart(true)
+NotificationHelper.cancelNotice()
+// 实时监控截图内容
+require('../lib/WebsocketCaptureHijack.js')()
+// 执行领取饲料
+let failToExecute = true
+try {
+  if (familySinger.enterFamily()) {
+
+    familySinger.openDrawer()
+    familySinger.feedFamily()
+    familySinger.feedOne()
+    familySinger.assignDuties()
+    familySinger.openDrawer()
+    let nextFeedTime = widgetUtils.widgetGetOne('\\d+点后 可来请客', 1000)
+    if (nextFeedTime) {
+      let label = nextFeedTime.text() || nextFeedTime.desc()
+      let checkResult = /(\d+)点.*/.exec(label)
+      if (checkResult) {
+        let remainTime = (parseInt(checkResult[1]) - new Date().getHours() - 1) * 60 + 1
+        let timestamp = new Date().getTime() + remainTime * 3600
+        commonFunctions.setUpAutoStart(remainTime)
+        NotificationHelper.createNotification('投喂完毕，等待下一次投喂', '下一次投喂时间：' + remainTime + '分钟后：' + dateFormat(new Date(timestamp)))
+      }
+    } else {
+      NotificationHelper.createNotification('蚂蚁庄园今日投喂完毕', '蚂蚁庄园家庭投喂完毕，明天再来')
+    }
+    failToExecute = false
+  }
+} catch (e) {
+  LogFloaty.pushErrorLog('脚本执行异常' + e)
+}
+if (failToExecute) {
+  LogFloaty.pushErrorLog('当前执行失败，设置五分钟后重试')
+  NotificationHelper.createNotification('家庭签到执行失败，请检查', '家庭签到执行失败，请检查是否存在问题')
+  commonFunctions.setUpAutoStart(5)
+}
+// setInterval(() => {}, 4000)
+commonFunctions.minimize()
+
+if (config.auto_lock === true && unlocker.needRelock() === true) {
+  debugInfo('重新锁定屏幕')
+  automator.lockScreen()
+  unlocker.saveNeedRelock(true)
+}
+runningQueueDispatcher.removeRunningTask(true)
+
+exit()
